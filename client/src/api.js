4ebebaeecdc52bde88db793205201a4b2a7a268a
Sampson Twene <twenesampson2@gmail.com.com>
2025-11-28 21:52:56 +0100

    Hardcode API base URL for production

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,12 @@
 import axios from 'axios';
 
+const baseURL =
+  import.meta.env.PROD
+    ? 'https://blogapp-9fre.onrender.com' // 🔴 put your Render URL here
+    : 'http://localhost:4000';
+
 export const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:4000',
+  baseURL,
 });
 
 API.interceptors.request.use((config) => {
